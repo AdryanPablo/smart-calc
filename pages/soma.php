025905e90fbd47bff31338b030dbedd27a347fad
Ádryan Pablo <apdsom22@gmail.com>
2023-09-27 19:19:23 -0300

    Create LinkButton and MainContent components

---
+++ pages/soma.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soma</title>
+
+    <link rel="stylesheet" href="../styles/components/components.css">
+    <link rel="stylesheet" href="../styles/generic/reset.css">
+</head>
+<body>
+
+    <a href="../index.html" class="link-button">Voltar</a>
+    
+</body>
+</html>
