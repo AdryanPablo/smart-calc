18e8715c34f7c5e0ad6a0ff8845226e96da90933
Ádryan Pablo <apdsom22@gmail.com>
2023-09-27 18:30:02 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Calc</title>
+</head>
+<body>
+
+    <?php
+
+        echo "Hello, world!"
+
+    ?>
+    
+</body>
+</html>
